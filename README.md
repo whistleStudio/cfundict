0807ba8fb26170ccfbf338ca51cd88ee4a0dd8fc
whistleStudio <whistleStudio@126.com>
2022-07-27 13:59:34 +0800

    07271358

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### [07261732] start
 
 vue3视频看完， uniapp默认vite编译，好像scss变量有点问题
+
+### [07271358]
+
+headbar
--- dictwx/components/headbar/headbar.vue
+++ dictwx/components/headbar/headbar.vue
@@ -1,25 +1,38 @@
 <template>
 	<view class="headbar"> 
-		<view v-if="1" class="default flex-row-center">
+		<view v-if="mode==0" class="default flex-row-center">
 			<view class="iconfont icon-icon_mulu"></view>
 			<view></view>
-			<view class="iconfont icon-sousuo"></view>
+			<view class="iconfont icon-sousuo" @click="mode=1"></view>
 		</view>
-		<view v-else class="search">
-			<view></view>
-			<view>Cancel</view>
+		<view v-else class="search flex-row-center">
+			<view>
+				<input v-model="keyword" type="text" placeholder="Search docs" placeholder-class="plh">
+				<text class="iconfont icon-sousuo"></text>
+				<text v-show="keyword" @click="keyword=''" 
+				class="iconfont icon-quxiao"></text>
+			</view>
+			<view @click="mode=0">Cancel</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import {reactive, toRefs} from "vue"
 	export default {
 		name:"headbar",
-		data() {
+		setup () {
+			let pageState = reactive({
+				mode: 0
+			})
+			let searchInfo = reactive({
+				keyword: ""
+			})
 			return {
-				
-			};
-		}
+				...toRefs(pageState),
+				...toRefs(searchInfo)
+			}
+		},
 	}
 </script>
 
